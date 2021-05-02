fd75102d6b475a8bfe53c2122df923af07de46d3
gokhandogan7 <gdogan3746@gmail.com>
2021-05-02 18:36:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const data = require('./db.json')
+const express = require('express');
+const app = express()
+const cors = require('cors')
+
+
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/api/dishes", (req, res)=>{
+    res.send(data.dishes)
+})
+app.get("/api/leaders", (req, res)=>{
+    res.send(data.leaders)
+})
+app.get("/api/promotions", (req, res)=>{
+    res.send(data.promotions)
+})
+app.get("/api/comments", (req, res)=>{
+    res.send(data.comments)
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+    console.log('server started on port'+ port)
+})
